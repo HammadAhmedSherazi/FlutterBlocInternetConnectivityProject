de6dd1806d2de8cf5f7a900c94c840358fb4b762
HammadAhmedSherazi <ahmedhammad262@gmail.com>
2023-12-12 03:12:02 +0500

    internet connectivity bloc

---
+++ lib/bloc/internet_bloc.dart/internet_state.dart
@@ -0,0 +1,9 @@
+abstract class  InternetState{}
+
+
+class InternetIntialState extends InternetState{}
+
+
+class InternetLostState extends InternetState{}
+
+class InternetGainedState extends InternetState{}
